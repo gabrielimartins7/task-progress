26392bb955767c884a0a61c1730cbee09a4423c5
martins.gabrieli.07.almeida@outlook.com <martins.gabrieli.07.almeida@outlook.com>
2025-05-12 17:13:59 -0300

    Criando validações no formulario

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -12,147 +12,170 @@ class _FormScreenState extends State<FormScreen> {
   TextEditingController difficultyController = TextEditingController();
   TextEditingController imageController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Adicionar tarefa'),
-        backgroundColor: Colors.orange,
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            height: 800,
-            width: 375,
-            decoration: BoxDecoration(
-              color: Colors.black12,
-              borderRadius: BorderRadius.circular(12),
-              border: Border.all(width: 2, color: Colors.black12),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: nameController,
-                      decoration: InputDecoration(
-                        labelText: 'Nome',
-                        filled: true,
-                        fillColor: Colors.white70,
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.orange, width: 2),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.deepOrange,
-                            width: 2,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      keyboardType: TextInputType.number,
-                      controller: difficultyController,
-                      decoration: InputDecoration(
-                        labelText: 'Dificuldade',
-                        filled: true,
-                        fillColor: Colors.white70,
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.orange, width: 2),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.deepOrange,
-                            width: 2,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      onChanged: (text) {
-                        setState(() {});
-                      },
-                      keyboardType: TextInputType.url,
-                      controller: imageController,
-                      decoration: InputDecoration(
-                        labelText: 'Imagem',
-                        filled: true,
-                        fillColor: Colors.white70,
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.orange, width: 2),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.deepOrange,
-                            width: 2,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 100,
-                    width: 80,
-                    margin: const EdgeInsets.symmetric(vertical: 12),
-                    decoration: BoxDecoration(
-                      color: Colors.orange,
-                      borderRadius: BorderRadius.circular(6),
-                      border: Border.all(width: 2, color: Colors.orange),
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(6),
-                      child: Image.network(
-                        imageController.text,
-                        errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
-                          return const Icon(
-                            Icons.broken_image,
-                            size: 48,
-                            color: Colors.black54,
-                          );
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Adicionar tarefa'),
+          backgroundColor: Colors.orange,
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              height: 800,
+              width: 375,
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(width: 2, color: Colors.black12),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        validator: (String? value){
+                          if(value != null && value.isEmpty){
+                            return 'Insira o nome da Tarefa';
+                          }
+                          return null;
                         },
-                        fit: BoxFit.cover,
+                        controller: nameController,
+                        decoration: InputDecoration(
+                          labelText: 'Nome',
+                          filled: true,
+                          fillColor: Colors.white70,
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.orange, width: 2),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.deepOrange,
+                              width: 2,
+                            ),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (nameController.text.isEmpty ||
-                          difficultyController.text.isEmpty ||
-                          imageController.text.isEmpty) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text('Por favor, preencha todos os campos!'),
-                            backgroundColor: Colors.red,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        validator: (value){
+                          if(value!.isEmpty
+                              || int.parse(value) > 5
+                              || int.parse(value) < 1){
+                            return 'Insira uma dificuldade entre 1 e 5';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.number,
+                        controller: difficultyController,
+                        decoration: InputDecoration(
+                          labelText: 'Dificuldade',
+                          filled: true,
+                          fillColor: Colors.white70,
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.orange, width: 2),
                           ),
-                        );
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text('Tarefa adicionada com sucesso!'),
-                            backgroundColor: Colors.green,
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.deepOrange,
+                              width: 2,
+                            ),
                           ),
-                        );
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orange,
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+                        ),
                       ),
-                      padding: EdgeInsets.symmetric(horizontal: 36, vertical: 18),
                     ),
-                    child: Text('Adicionar'),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        onChanged: (text) {
+                          setState(() {});
+                        },
+                        validator: (value){
+                          if(value!.isEmpty){
+                            return 'Insira uma URL de imagem';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.url,
+                        controller: imageController,
+                        decoration: InputDecoration(
+                          labelText: 'Imagem',
+                          filled: true,
+                          fillColor: Colors.white70,
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.orange, width: 2),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.deepOrange,
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                      width: 80,
+                      margin: const EdgeInsets.symmetric(vertical: 12),
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(6),
+                        border: Border.all(width: 2, color: Colors.orange),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(6),
+                        child: Image.network(
+                          imageController.text,
+                          errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                            return const Icon(
+                              Icons.broken_image,
+                              size: 48,
+                              color: Colors.black54,
+                            );
+                          },
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Tarefa adicionada com sucesso!'),
+                              backgroundColor: Colors.green,
+                            ),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Por favor, corrija os erros do formulário.'),
+                              backgroundColor: Colors.red,
+                            ),
+                          );
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+                      ),
+                      child: Text('Adicionar'),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
